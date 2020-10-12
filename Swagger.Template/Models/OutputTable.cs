cfd8996bfc5f3093f2b2d8835a2a092e9d286d06
yunqian44 <yunqian8@live.com>
2020-10-12 21:33:20 +0800

    add api output model

---
+++ Swagger.Template/Models/OutputTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger.Template.Models
+{
+    public class OutputTable
+    {
+        /// <summary>
+        /// API Title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// API Tag
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// APT Url
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// API Description
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
